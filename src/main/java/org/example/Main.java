e55106768349880d6bbcdcc9cd24f91c73f6d70c
Gustavo de oliveira <71619573+Gustavoo151@users.noreply.github.com>
2024-04-09 14:14:43 -0300

    Trabalhando com textos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,15 @@ public class Main {
         System.out.println("Filme Top Gun");
 
         int anoDeLacamento = 2020;
-
         System.out.println("Ano de lançamento" + anoDeLacamento);
 
         boolean incluidoNoPlano = true;
-
         double nota = 8.1;
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println("Media do Filme: " + media);
+
+        String sinopse;
+        sinopse = "Filme de aventura com galãm dos anos 80";
+        System.out.println(sinopse);
     }
 }
