dc588b8ca02693e665c3fbdaac012670f9cd315d
Gustavo de oliveira <71619573+Gustavoo151@users.noreply.github.com>
2024-04-09 14:02:51 -0300

    02-Atribuindo valores ao filme

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        System.out.println("Esse é o Scream Match");
+        System.out.println("Filme Top Gun");
+
+        int anoDeLacamento = 2020;
+
+        System.out.println("Ano de lançamento" + anoDeLacamento);
+
+        boolean incluidoNoPlano = true;
+
+        double nota = 8.1;
     }
 }
